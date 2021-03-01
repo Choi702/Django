78188d1fa13c0d424a71b6a0de3a9c94ef597c6f
Choi702 <jaeychoi1@gmail.com>
2021-03-01 14:36:02 -0800

    django complete test complete

---
+++ django_snack/snacks/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import homepage, aboutpage
+
+urlpatterns = [
+    path('', homepage.as_view(), name = 'home'),
+    path('about', aboutpage.as_view(), name = 'about')
+   
+]
